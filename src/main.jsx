af5567f1afe5a2ede2d796eecd0cec6b0f097b08
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-09-01 20:21:05 +0600

    feat: add routing with react-router-dom and define application routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/route.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>
 );
