e44b90a916712fc2889ed04a6389d53dee28007e
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-09-08 09:46:08 +0600

    feat: refactor ProfilePage to use Profile context for state management and remove local state

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,37 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import useAxios from "../hooks/useAxios";
 import { useAuth } from "../hooks/useAuth";
+import useProfile from "../hooks/useProfile";
+import { actions } from "../actions";
 
 const ProfilePage = () => {
-  const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { state, dispatch } = useProfile();
 
   const { api } = useAxios();
   const { auth } = useAuth();
 
   useEffect(() => {
-    setLoading(true);
+    dispatch({ type: actions.profile.DATA_FETCHING });
     const fetchProfile = async () => {
       try {
         const response = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`);
-        setUser(response?.data?.user);
-        setPosts(response?.data?.posts);
+        dispatch({ type: actions.profile.DATA_FETCHED, data: response.data });
       } catch (err) {
         console.error(err);
-        setError(err);
-      } finally {
-        setLoading(false);
+        dispatch({ type: actions.profile.DATA_FETCH_ERROR, error: err.message });
       }
     };
 
     fetchProfile();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (state?.loading) return <div>Loading...</div>;
 
-  return <div className="text-white">{user?.firstName}</div>;
+  return (
+    <div className="text-white">
+      {state?.user?.firstName}
+      <p>Posts: {state?.posts?.length}</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
